e269e31bb7338393d69124c218d2482c18ed83f4
Sara Garcia <sara.garcia.g@avanade.com>
2021-02-11 13:30:43 +0100

    +credentials
    aa

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,6 +1,6 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+    
     stages {
         stage('Checkout') {
             steps {
@@ -11,21 +11,6 @@ pipeline {
             steps {
                 sh './mvnw clean package'
                 //sh 'false' // true
-            }
-        
-            post {
-                always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
-                }
             }
         }
     }
